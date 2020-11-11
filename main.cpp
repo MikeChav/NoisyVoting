faabc933e94a69cf22fd70d15231fba11923de06
Michael Chavrimootoo <mcchavrimootoo@gmail.com>
2020-11-10 20:44:34 -0500

    Implemented RMI Mallows sampling, but it returns a different distribution

--- main.cpp
+++ main.cpp
@@ -50,8 +50,37 @@ order get_sample(_distribution pi, double r) { // O(m!)
     }
     return last; // assuming some slight accuracy error happened and we're in the range for the last number
 }
+int ZZ = 0;
+int ZY = 0;
+int ZX = 0;
+int ZA = 0;
 
-vector<int> evaluate_winner(const vector<order> &V) { //O(n) assuming plurality
+order estimate_sample(_order pi, double dispersion, default_random_engine &engine, uniform_real_distribution<double> &unif) {
+    double denom = 0.0;
+    int m = pi.size();
+    order sample;
+    for (int i = 0; i < m; i++) {
+        denom += pow(dispersion, i);
+        double r = unif(engine)*denom;
+        double total = 0.0; bool place = false;
+        for (int j = 0; j <= i; j++) {
+            total += pow(dispersion, i-j);
+            if (r <= total) {
+                if (j == 1) ZZ++;
+                else ZY++;
+                if (i==j) ZA++;
+                sample.insert(sample.begin()+j, pi[i]);
+                place = true;
+                break;
+            }
+        }
+        if (!place) ZX++; // from this tracker, it seems likely that this case is never hit, but it's good to have as safety
+        if (!place) sample.push_back(pi[i]); // under assumption that no placement was made due to numerical instability
+    }
+    return sample;
+}
+
+vector<int> evaluate_winners(const vector<order> &V) { //O(n) assuming plurality, could be any other P-time election
     vector<int> counts(V.size(), 0);
     for (const auto & v : V) {
         counts[v[0]-1]++;
@@ -84,11 +113,12 @@ int main() { //O((n/e)^2 log(1/delta) (m!*n + 2n))
     cin >> p >> eps >> delta;
 
     N = (n*n*log(1.0/delta))/(eps*eps); // todo reorder for optimality
+    cout << N << endl;
 
-    vector<distribution> distributions(n);
-    for (int i = 0; i < distributions.size(); i++) { // O(m!)
-        distributions[i] = compute_distribution(dispersions[i], V[i]);
-    }
+//    vector<distribution> distributions(n);
+//    for (int i = 0; i < distributions.size(); i++) { // O(m!)
+//        distributions[i] = compute_distribution(dispersions[i], V[i]);
+//    }
 
     default_random_engine engine(chrono::system_clock::now().time_since_epoch().count());
     uniform_real_distribution<double> unif(0.0, 1.0);
@@ -96,13 +126,20 @@ int main() { //O((n/e)^2 log(1/delta) (m!*n + 2n))
     for (int j = 0; j < N; j++) {
         vector<order> sample(n);
         for (int i = 0; i < n; i++) {
-            sample[i] = get_sample(distributions[i], unif(engine)); //O(m!)
+//            sample[i] = get_sample(distributions[i], unif(engine), engine, unif); //O(m!)
+            sample[i] = estimate_sample(V[i], dispersions[i], engine, unif); //O(m^2)
         }
-        auto winners = evaluate_winner(sample);
+        auto winners = evaluate_winners(sample);
         for (auto w : winners) results[w-1]++;
-
     }
-    for (int i = 0; i < m; i++)
-        cout << "Probability " << i+1 << " wins is: "<< double(results[i])/double(N) << endl;
+    for (int i = 0; i < m; i++) cout <<  results[i] << " "; cout << endl;
+    for (int i = 0; i < m; i++) {
+        cout << "Probability " << i + 1 << " wins is: " << double(results[i]) / double(N) << endl;
+    }
+//    cout << ZZ << endl;
+//    cout << ZY << endl;
+    cout << ZX << endl;
+    cout << ZA-ZZ-ZY<< endl;
+
     return 0;
 }
