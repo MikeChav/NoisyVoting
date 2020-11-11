4d249efdb515abe3277f88770c8a3184e0979610
Michael Chavrimootoo <mcchavrimootoo@yahoo.com>
2020-11-11 13:47:36 -0500

    Fixed bug

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <map>
-#include <numeric>
 #include <algorithm>
 #include <random>
 #include <chrono>
@@ -9,12 +8,10 @@
 using namespace std;
 
 typedef vector<int> order;
-typedef map<order, double> distribution;
-
 #define _order const order &
-#define _distribution const distribution &
 
 
+// todo make this into kendall-tau and adapt sampling accordingly
 long dist(_order pi, _order pi_0) { //O(|pi|) = O(m), can be any poly(m) rule
     long total = 0;
     for (int i = 0; i < pi.size(); i++) {
@@ -23,53 +20,15 @@ long dist(_order pi, _order pi_0) { //O(|pi|) = O(m), can be any poly(m) rule
     return total;
 }
 
-distribution compute_distribution(double disp, _order pi_0) { //O(m!)
-    // todo potentially improve by running this m! factorial step once
-    vector<int> pi(pi_0.size());
-    distribution  distrib;
-    iota(pi.begin(), pi.end(), 1);
-    double total = 0.0;
-    do {
-        double p = pow(exp(-disp), dist(pi, pi_0)); // todo check if best way to do this
-        total += p;
-        distrib[pi] = p;
-    } while (next_permutation(pi.begin(), pi.end()));
-    for (auto & di : distrib) {
-        di.second /= total;
-    }
-    return distrib;
-}
-
-order get_sample(_distribution pi, double r) { // O(m!)
-    double total = 0.0;
-    order last;
-    for (const auto& p : pi) { // making use of the fact that map is ordered
-        total += p.second;
-        last = p.first;
-        if (r <= total) return last;
-    }
-    return last; // assuming some slight accuracy error happened and we're in the range for the last number
-}
-int ZZ = 0;
-int ZY = 0;
-int ZX = 0;
-int ZA = 0;
-
-
 int get_insert_location(int i, double dispersion, double r) { // <<-- the bug must be here // O(m)
     double total = 0.0;
     for (int j = 0; j < i; j++) {
         total += pow(dispersion, i-j);
         if (r <= total) {
-            ((j==1)?ZZ:ZY)++;
-//            if (j == 1) ZZ++;
-//            else ZY++;
-            ZA += (i==j);
             return j;
         }
     }
-    ZX++; // from this tracker, it seems likely that this case is never hit, but it's good to have as safety
-    return i;  // under assumption that no placement was made due to numerical instability
+    return i;  // under assumption that no placement was made due to numerical instability, just add at the end
 }
 
 order estimate_sample(_order pi, double dispersion, default_random_engine &engine, uniform_real_distribution<double> &unif) { // O(m^2)
@@ -102,29 +61,25 @@ int main() { //O((n/e)^2 log(1/delta) (m^2*n + 2n))
     int n /* num_voters */, m /* num_candidates */, p /* distinguised candidate */;
     long N, C = 0;
     double eps, delta;
+    default_random_engine engine(chrono::system_clock::now().time_since_epoch().count());
+    uniform_real_distribution<double> unif(0.0, 1.0);
     cin >> m >> n;
+
+    // in theory this variable is not needed, but I'm trying to get the whole distribution from the simulation
+    vector<int> results(m, 0);
     vector<double> dispersions(n);
-    vector<order> V(n); // V[i][j] indicates voter i's (j+1)'s favorite candidate
+    vector<order> V(n); // V[i][j] indicates voter (i+1)'s (j+1)'s favorite candidate
+
     for (int i = 0; i < n; i++) { // O(nm)
         for (int j = 0; j < m; j++) {
-            int vote; cin >> vote;
+            int vote; cin >> vote; // assume a full-order (no ties)
             V[i].push_back(vote);
         }
         cin >> dispersions[i];
     }
     cin >> p >> eps >> delta;
+    N = n*n*log(1.0/delta)/(eps*eps); // todo reorder for optimality
 
-    N = (n*n*log(1.0/delta))/(eps*eps); // todo reorder for optimality
-    cout << N << endl;
-
-//    vector<distribution> distributions(n);
-//    for (int i = 0; i < distributions.size(); i++) { // O(m!)
-//        distributions[i] = compute_distribution(dispersions[i], V[i]);
-//    }
-
-    default_random_engine engine(chrono::system_clock::now().time_since_epoch().count());
-    uniform_real_distribution<double> unif(0.0, 1.0);
-    vector<int> results(m, 0);
     for (int j = 0; j < N; j++) { // O(Nm^2)
         vector<order> sample(n);
         for (int i = 0; i < n; i++) {
@@ -132,16 +87,12 @@ int main() { //O((n/e)^2 log(1/delta) (m^2*n + 2n))
             sample[i] = estimate_sample(V[i], dispersions[i], engine, unif); //O(m^2)
         }
         auto winners = evaluate_winners(sample); // poly(n, m)
-        for (auto w : winners) results[w-1]++; // O(m)
+        for (auto w : winners) results[w]++; // O(m)
     }
     for (int i = 0; i < m; i++) cout <<  results[i] << " "; cout << endl;
     for (int i = 0; i < m; i++) {
         cout << "Probability " << i + 1 << " wins is: " << double(results[i]) / double(N) << endl;
     }
-//    cout << ZZ << endl;
-//    cout << ZY << endl;
-    cout << ZX << endl;
-    cout << ZA-ZZ-ZY<< endl;
 
     return 0;
 }
